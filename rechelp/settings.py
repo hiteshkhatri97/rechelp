8b07b237862a7e1912ba5a9bdf27a818045a6707
Mayur Punjabi <mayurpunjabi9@gmail.com>
2019-04-10 23:12:13 +0530

    Update for production
    update secret key and debug in settings.py

--- rechelp/settings.py
+++ rechelp/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yjq_3b*@^me(9serj10o^in8_@(2qr(82^m@s20a9s+4@%k9uj'
+# SECRET_KEY = 'yjq_3b*@^me(9serj10o^in8_@(2qr(82^m@s20a9s+4@%k9uj'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','yjq_3b*@^me(9serj10o^in8_@(2qr(82^m@s20a9s+4@%k9uj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
 ALLOWED_HOSTS = []
 
 
